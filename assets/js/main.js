44add6f8da41ef134db45d31d29707b7c2dc5b98
Aziz <webmaster4443@gmail.com>
2023-08-25 16:27:49 +0500

    Начало работы в Гите

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+  
+  
+/* Здесь мы настроили через параматеры наш слайдер
+
+owl.owlCarousel({
+    center: true, //Centrenirovanie elementa
+    loop: true, //Vklyu4ili sikl
+    margin: 30, // Dali otstupi
+    startPosition: 1, //na4netsy s ne 0, a s 1 
+    items: 3, // za raz pokazivat 3 slidera
+    
+}); Здесь параметры слайдера 
+
+А вот здесь кнопки наши их классы им задали чтоб при
+клике они крутились в определенную, сторону
+
+$('.slider__btn__prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+});
+
+
+$('.slider__btn__next').click(function() {
+    owl.trigger('next.owl.carousel');
+});
+
+*/
+
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true, //Centrenirovanie elementa
+    loop: true, //Vklyu4ili sikl
+    margin: 30, // Dali otstupi
+    startPosition: 1, //na4netsy s ne 0, a s 1 
+    items: 3, // za raz pokazivat 3 slidera
+    
+});
+
+$('.slider__btn__prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+});
+
+
+$('.slider__btn__next').click(function() {
+    owl.trigger('next.owl.carousel');
+});
